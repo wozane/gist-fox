f854464f7d420610afa7138bea1c32a514dd7288
Aneta Wozniak <wozniakaneta5@gmail.com>
2017-10-09 14:27:39 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## create a react app with webpack
+
+1. yarn init
+2. yarn add webpack webpack-dev-server path
+3. yarn add -D babel-loader babel-core babel-present-env babel-preset-es2015 babel-preset-react
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+// const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.jsx',
+  output: {
+    filename: './dist/bundle-[hash].js',
+  },
+  module: {
+    loaders: [
+      { test: /\.jsx$/, use: ['babel-loader', 'eslint-loader'] },
+      { test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader'] },
+    ],
+  },
+  resolve: {
+    extensions: ['.jsx', '.js', '.json'],
+  },
+  /* plugins: [new HtmlWebpackPlugin({
+    template: './src/index.html',
+  })],
+  */
+  devtool: 'source-map',
+  devServer: {
+    contentBase: './src',
+    historyApiFallback: true,
+    // noInfo: true,
+  },
+}
