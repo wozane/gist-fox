50f530f266cb3b9bbeeb97d44fb6b8f70b0bae8d
Aneta Wozniak <wozniakaneta5@gmail.com>
2017-10-10 11:15:53 +0200

    Mock up version of the app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,10 +3,22 @@ import React from 'react'
 
 const App = () => (
   <div>
-    <h1>This is gist</h1>
-    <main>
-      <span>Lorem ipsum</span>
+    <nav className="navbar is-dark has-shadow">Gist nav bar menu</nav>
+    <section> list of available gists </section>
+    <main className="container">
+      <section>Gist body
+        <div>Gist description</div>
+        <div>gist main
+          <nav>toolbar
+            <div>file name extension</div>
+            <div>writing options</div>
+          </nav>
+          <text>body of the gist</text>
+        </div>
+        <section>buttons</section>
+      </section>
     </main>
+    <section>Footer</section>
   </div>
 )
 
