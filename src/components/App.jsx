1cf954624b8f8159b8a92d3aabfbe88ecf3ce075
Aneta Wozniak <wozniakaneta5@gmail.com>
2017-10-10 11:46:53 +0200

    Add Nabar & Gist components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,13 @@
 import React from 'react'
+
+import Navbar from './Navbar'
+import Gist from './Gist'
 // import { BrowserRouter } from 'react-router-dom'
 
 const App = () => (
   <div>
-    <nav className="navbar is-dark has-shadow">Gist nav bar menu</nav>
-    <section> list of available gists </section>
-    <main className="container">
-      <section>Gist body
-        <div>Gist description</div>
-        <div>gist main
-          <nav>toolbar
-            <div>file name extension</div>
-            <div>writing options</div>
-          </nav>
-          <text>body of the gist</text>
-        </div>
-        <section>buttons</section>
-      </section>
-    </main>
-    <section>Footer</section>
+    <Navbar />
+    <Gist />
   </div>
 )
 
