64f871f30efe022132cccf59d97814f39f6cd511
Aneta Wozniak <wozniakaneta5@gmail.com>
2017-10-13 12:07:08 +0200

    Change the mock up of the app

--- src/components/GistFooter.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const GistFooter = () => (
-  <footer> Gist Footer </footer>
-)
-
-export default GistFooter
--- src/components/Navbar.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Navbar = () => (
-  <nav className="navbar is-dark">navbar menu </nav>
-)
-
-export default Navbar
