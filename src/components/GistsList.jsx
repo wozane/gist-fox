fdf93d31ff0b4dfa9f70eac2f5ad271b3d5b9d0a
Aneta Wozniak <wozniakaneta5@gmail.com>
2017-10-16 15:02:14 +0200

    Change GistList to class component - wip

--- src/components/GistsList.jsx
+++ src/components/GistsList.jsx
@@ -1,15 +1,36 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 import GistHeader from './GistHeader'
 
-const Gist = ({ match }) => (
-  <div className="gist-list">
-    <GistHeader />
-    <Link to={`${match.url}/32234234`} href={`${match.url}/32234234`}>
-      dasdsad
-    </Link>
-  </div>
-)
+class GistList extends React.Component {
+  constructor() {
+    super()
 
-export default Gist
+    this.state = {
+      gists: [],
+    }
+  }
+
+  componentDidMount() {
+    const apiUrl = 'https://private-anon-dc77e86d57-awapp.apiary-mock.com/gists'
+    axios.get(apiUrl)
+      .then((response) => {
+        const gists = response.data._embedded.gists.map(gist => gist.description)
+        this.setState({ gists })
+      })
+  }
+
+  render() {
+    return (
+      <div className="gist-list">
+        <GistHeader />
+        <ul>
+          {this.state.gists.map(gist => <li key={gist.id}>{gist.description}</li>)}
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default GistList
