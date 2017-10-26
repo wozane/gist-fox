6581939cac1b49cd5bb3aacd73e7e727b050e2b9
Aneta Wozniak <wozniakaneta5@gmail.com>
2017-10-26 14:55:34 +0200

    Add button option to 'unstar gist'

--- src/components/Gist.jsx
+++ src/components/Gist.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
 
 export default function (props) {
-  console.log(props.openEditForm)
+  const starButton = props.star ? 'Star' : 'Unstar'
   return (
     <div>
       <h2>{props.gist.description}</h2>
       <p>Date: {props.gist.creaded_at}</p><br />
       <p>{props.gist.content}</p>
-      <button
-        className="button is-warning"
-        onClick={props.starGist}
-      >
-        Give it a star
-      </button>
+      <div>
+        <button
+          className="button is-warning"
+          onClick={props.starGist}
+        >
+          {starButton}
+        </button>
+      </div>
       <button
         className="button is-primary"
         onClick={props.openEditForm}
