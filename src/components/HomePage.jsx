b6d9e874cf858ba89a9e02ff0b011ec39bf1c03d
Aneta Wozniak <wozniakaneta5@gmail.com>
2017-10-27 10:27:13 +0200

    Add styles to home page

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const HomePage = () => (
-  <div>
-    <header> Welcome! </header>
-    <Link to="/gists">
-      Gists lists
-    </Link>
+  <div className="container">
+    <header className="title"> Welcome! </header>
+    <div className="content is-large">
+      <Link to="/gists">
+        Gists lists
+      </Link>
+    </div>
   </div>
 )
 
